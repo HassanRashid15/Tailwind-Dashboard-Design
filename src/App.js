b4723ca938d1480e77ee5b15afb954031bffa112
HassanRashid15 <hassanrashid0018@gmail.com>
2024-08-21 04:05:24 -0700

    Dashboard UI Design

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-200">
+    <h1 className="text-4xl font-bold text-red-500">Hello, Tailwind CSS!</h1>
+  </div>
+  );
+}
+
+export default App;
